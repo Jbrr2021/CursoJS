8c3d0ebbf660fe3d65e8440fb0f68daffc3c2bf6
João Batista Rodrigues Ribeiro <87319898+Jbrr2021@users.noreply.github.com>
2024-07-10 22:14:27 -0300

    Aula 14 e !5 com exercicio.

--- aula14/index.js
+++ aula14/index.js
@@ -2,9 +2,9 @@
 let num1 = 0.7; // number
 let num2 = 0.1; // number
 
-num1 = ((num1 *100) + (num2 * 100)) / 100;
-num1 = ((num1 *100) + (num2 * 100)) / 100;
-num1 = ((num1 *100) + (num2 * 100)) / 100;
+//num1 = ((num1 *100) + (num2 * 100)) / 100;
+//num1 = ((num1 *100) + (num2 * 100)) / 100;
+//num1 = ((num1 *100) + (num2 * 100)) / 100;
 
 num1 = parseFloat(num1.toFixed(2)); // pode ser utulizado Number também.
 
---
+++ aula15_ObjetoMath/index.js
@@ -0,0 +1,7 @@
+let num1 = 9.51;
+//let num2 = Math.floor(num1);Arendonda para baixo
+//let num2 = Math.ceil(num1); Aredonda para cima
+//let num2 = Math.round(num1); Arendonda de forma automatica
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); Pegar o maior numero lista de numeros.
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); Pegar o Menor numeor da lista.
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); Gera numeros aleatorios.
